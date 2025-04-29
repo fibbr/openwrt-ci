93c62e3604b7385cfbc49945e7bc46127a8a62cb
fibbr <106511107+fibbr@users.noreply.github.com>
2025-04-30 01:35:44 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -39,11 +39,8 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
-git clone https://github.com/Boos4721/openwrt
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-make menuconfig
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/kenzok8/small-package.git package/luci-app-clouddrive2
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small.git package/openwrt-small
 
 # 科学上网插件
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
